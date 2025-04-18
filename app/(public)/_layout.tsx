ebc0c95557cc00272182dbb3ed36c5383c7059fd
A-4-Atom <vcikas2626@gmail.com>
2025-04-18 16:40:25 +0530

    Initial Commit
    Added Context, and user authentication with clerk.

---
+++ app/(public)/_layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const PublicLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#6c47ff" },
+        headerTintColor: "#fff",
+        headerBackTitle: "Back",
+      }}
+    >
+      <Stack.Screen name="login" options={{ headerTitle: "Quick Alert" }} />
+      <Stack.Screen
+        name="register"
+        options={{ headerTitle: "Create Account" }}
+      />
+      <Stack.Screen name="reset" options={{ headerTitle: "Reset Password" }} />
+    </Stack>
+  );
+};
+export default PublicLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,62 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import "../global.css";
+import GlobalProvider from "@/context/GlobalProvider";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { Slot, useRouter, useSegments } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      console.error("Error getting token:", error);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      await SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      console.error("Error saving token:", error);
+      return;
+    }
+  },
+};
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+function InitialLayout() {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if(!isLoaded) return;
+
+    const inTabsGroup = segments[0] === "(auth)";
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace("/home");
+    } else if (!isSignedIn) {
+      router.replace("/login");
     }
-  }, [loaded]);
+  }, [isSignedIn])
 
-  if (!loaded) {
-    return null;
-  }
+  if(!isLoaded) return null;
 
+  return <Slot/>;
+}
+
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <>
+      <ClerkProvider tokenCache={tokenCache}>
+        <GlobalProvider>
+          <InitialLayout />
+          <StatusBar backgroundColor="#161622" style="light" />
+        </GlobalProvider>
+      </ClerkProvider>
+    </>
   );
 }
